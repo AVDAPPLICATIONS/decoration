2b17053771076164db923cc30f23bd15cd5a1238
Krutarth Solanki <119230368+Krut369@users.noreply.github.com>
2025-09-22 12:29:21 +0530

    Remove extra files

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,5 @@
 const String appName = "Your App Name";
-const String apiBaseUrl = "https://ad27f8b700b4.ngrok-free.app";
+const String apiBaseUrl = "https://608964385de0.ngrok-free.app";
 
 const int defaultTimeout = 5000; // in milliseconds
 
